332038d9bd7fc740ad95a002e71e4c62d9e5da18
Ottey <kadokawa.tomoki@gmail.com>
2020-07-15 17:50:11 +0900

    neko

---
+++ 3Dshooting/Assets/Ottey/scripts/EnemyGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject enemyPrefab;
+
+
+    const int GENERATE_INTERVAL_F = 120;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        StartCoroutine("ManageInterval");
+    }
+
+
+    IEnumerator ManageInterval()
+    {
+        for (int i = 0; i < GENERATE_INTERVAL_F; i ++)
+        {
+            GenerateEnemy();
+            yield return null;
+        }
+    }
+
+
+    void GenerateEnemy()
+    {
+        Instantiate(enemyPrefab);
+    }
+}
